26da9e0f841a9e7ecee18ef7084b65c342c288bf
AK <mail2akvs@gmail.com>
2021-05-06 14:09:58 +0530

    Hi

---
+++ src/org/student/Marks.java
@@ -0,0 +1,22 @@
+package org.student;
+
+public class Marks {
+public static void main(String[] args) {
+	int id=202178;
+	System.out.println("StudentId: "+id);
+	String name="AK";
+	System.out.println("StudentName: "+name);
+	byte mark1=70;
+	System.out.println("Mark1: "+mark1);
+	byte mark2=89;
+	System.out.println("Mark2: "+mark2);
+	byte mark3=89;
+	System.out.println("Mark2: "+mark3);
+	byte mark4=89;
+	System.out.println("Mark2: "+mark4);
+	int tot=mark1+mark2+mark3+mark4;
+	System.out.println("Total: "+tot);
+	int avg=tot/4;
+	System.out.println("Average: "+avg);
+}
+}
---
+++ src/org/student/StudentDetail.java
@@ -0,0 +1,33 @@
+package org.student;
+
+import java.util.Scanner;
+
+public class StudentDetail {
+public static void main(String[] args) {
+	Scanner a=new Scanner(System.in);
+	System.out.println("Enter student name");
+	String next = a.next();
+	System.out.println("Student name is "+next);
+	
+	System.out.println("Enter your Id");
+	int nextInt = a.nextInt();
+	System.out.println("Student Id is"+nextInt);
+	
+	System.out.println("Enter student dept");
+	String next2 = a.next();
+	System.out.println("Dept : "+next2);
+	
+	System.out.println("Enter your mob no.");
+	long nextLong = a.nextLong();
+	System.out.println("Mob no is "+nextLong);
+	
+	System.out.println("Gender");
+	String next3 = a.next();
+	System.out.println("Gender is "+next3);
+	
+	System.out.println("Enter city");
+	String next4 = a.next();
+	System.out.println("City is "+next4);
+	
+}
+}
